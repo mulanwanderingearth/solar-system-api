c756af8ced162fb8a79f02797c41e767678d3329
Olga-allgood <olga.s.orlova@gmail.com>
2025-10-20 11:27:44 -0700

    add wave two

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planet import planets
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
@@ -15,3 +15,29 @@ def get_all_planets():
             "has_life": planet.has_life 
         })
     return result_list
+
+def validate_planet(planet_id):
+    try: 
+        planet_id = int(planet_id)
+    except ValueError:
+        invalid_response = {"message": f"Planet id {planet_id} is invalid. Must be an integer."} 
+        abort(make_response(invalid_response, 400))   
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    not_found_response = {"message": f"Planet id {planet_id} not found"} 
+    abort(make_response(not_found_response, 404))
+
+@planet_bp.get("/<planet_id>")
+def get_single_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "size": planet.size,
+        "description": planet.description,
+        "has_life": planet.has_life
+    }
+
