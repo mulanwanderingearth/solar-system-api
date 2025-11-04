d56fa9e41fcf3ed91899fe72261b5ed831bc2aa3
Maya Zakharova <mayzakharova@gmail.com>
2025-11-04 14:28:03 -0600

    update table moon

--- app/models/moon.py
+++ app/models/moon.py
@@ -10,6 +10,9 @@ if TYPE_CHECKING:
 class Moon(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
+    size: Mapped[int]
+    description: Mapped[Optional[str]]
+    has_life: Mapped[bool] = mapped_column(default=False)
     planet_id: Mapped[Optional[int]] = mapped_column(ForeignKey("planet.id"))
     planet: Mapped[Optional["Planet"]] = relationship(back_populates="moons")
 
@@ -18,12 +21,18 @@ class Moon(db.Model):
         return {
             "id": self.id,
             "name": self.name,
+            "size": self.size,
+            "description": self.description,
+            "has_life": self.has_life,
             "planet": self.planet.name if self.planet_id else None
         }
     
     @classmethod
     def from_dict(cls, moon_data):
         return cls(
-            name=moon_data["name"],
-            planet_id=moon_data.get("planet_id")
-        )
+        name=moon_data["name"],
+        size=moon_data["size"],
+        description=moon_data.get("description"),
+        has_life=moon_data.get("has_life", False),
+        planet_id=moon_data.get("planet_id")
+    )
